946cfb27b89f9c46c03db528205d2fe93e899980
ashan0901 <ashanchanuka1211@gmail.com>
2024-06-04 12:14:54 +0530

    Add social media links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,7 @@
 import { CONTACT } from "../constants";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -11,6 +14,31 @@ const Contact = () => {
         <p href="#" className="my-4">
           {CONTACT.email}
         </p>
+        <div className="flex flex-wrap items-center justify-center gap-4 lg:mt-12 lg:mb-24">
+          <a
+            href="https://www.facebook.com/profile.php?id=100008487134621&mibextid=LQQJ4d"
+            target="_blank"
+          >
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+              <FaFacebook className="text-7xl text-white-500" />
+            </div>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/ashan-fernando-06960b222/"
+            target="_blank"
+          >
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+              <FaLinkedin className="text-7xl text-white-600" />
+            </div>
+          </a>
+
+          <a href="https://github.com/ashan0901" target="_blank">
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+              <FaGithub className="text-7xl text-white-800" />
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
